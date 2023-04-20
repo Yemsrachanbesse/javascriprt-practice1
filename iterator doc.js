08ecc74c0ea805c5c29e7594fa0ca1c6b76f136f
Yemsrachanbesse <yemsrach.anbesse@gmail.com>
2023-04-20 20:58:11 +0300

    eddited

--- iterator doc.js
+++ iterator doc.js
@@ -3,7 +3,7 @@ const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
 // Something is missing in the method call below
 
 console.log(words.some(word => {
-  return word.length < 6;
+  return word.length < = 6;
 }));
 
 // Use filter to create a new array
