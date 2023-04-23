e20c80d96f790e92a391dde2587d36d1d5c6560c
Yemsrachanbesse <yemsrach.anbesse@gmail.com>
2023-04-23 19:21:07 +0300

    edited

--- iterator doc.js
+++ iterator doc.js
@@ -10,6 +10,6 @@ console.log(words.some(word => {
 const interestingWords = words.filter((word) => {return word.length > 5});
 
 
-// Make sure to uncomment the code below and fix the incorrect code before running it
+// Make sure to uncomment the code below and fix the incorrect  code before running it 
 
 console.log(interestingWords.every((word) => {return word.length > 5}));
